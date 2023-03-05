218f3250095ab4260f8c3429e81eb248b7f8b494
igoogolx <27353191+igoogolx@users.noreply.github.com>
2023-03-04 19:11:23 -0800

    refactor: remove unused code

--- scripts/constants.ts
+++ scripts/constants.ts
@@ -1,5 +1,4 @@
 import * as path from "path";
-import * as os from "os";
 
 export const MODULES_PATH = path.join(process.cwd(), "modules");
 export const DASHBOARD_PATH = path.join(MODULES_PATH, "lux-dashboard");
