d83896009d3a88a25163a732219a2479c24c6b3e
igoogolx <27353191+igoogolx@users.noreply.github.com>
2023-08-13 02:37:31 -0700

    refactor: remove old project

--- scripts/constants.ts
+++
@@ -1,9 +0,0 @@
-import * as path from "path";
-
-export const MODULES_PATH = path.join(process.cwd(), "modules");
-export const ITUN2SOCKS_PATH = path.join(MODULES_PATH, "itun2socks");
-export const CLIENT_PATH = path.join(MODULES_PATH, "lux-client");
-export const THIRD_PARTIES_PATH = path.join(process.cwd(), "third_parties");
-export const CORE_DIR_NAME = "core";
-export const TMP_DIR = "tmp";
-export const OUT_PATH = "out";
