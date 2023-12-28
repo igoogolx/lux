97f237e4066b0d8fab81526d6950bc4bf091956d
igoogolx <27353191+igoogolx@users.noreply.github.com>
2023-12-28 23:36:27 +0800

    feat: upgrade core

--- scripts/init.dart
+++ scripts/init.dart
@@ -15,7 +15,7 @@ final assetsPath =
 final binDir = Directory(path.join(assetsPath, 'bin'));
 
 const rawCoreName = 'itun2socks';
-const rawCoreVersion = '0.16.4';
+const rawCoreVersion = '0.16.5';
 
 Future downloadLatestCore(String arch) async {
   var releaseArch = LuxCoreName.arch;
