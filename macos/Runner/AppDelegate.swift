d64e59bd79f30642cb7340fadc5887b4c472548c
igoogolx <27353191+igoogolx@users.noreply.github.com>
2024-09-01 13:37:13 +0800

    feat(MacOS): handle reopen

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -14,4 +14,17 @@ class AppDelegate: FlutterAppDelegate {
     let controller : FlutterViewController = mainFlutterWindow?.contentViewController as! FlutterViewController
     return _windowManager.applicationShouldTerminate(controller);
     }
+
+    override func applicationShouldHandleReopen(_ sender: NSApplication, hasVisibleWindows flag: Bool) -> Bool {
+            if !flag {
+                for window in NSApp.windows {
+                    if !window.isVisible {
+                        window.setIsVisible(true)
+                    }
+                    window.makeKeyAndOrderFront(self)
+                    NSApp.activate(ignoringOtherApps: true)
+                }
+            }
+            return true
+        }
 }
