2e5e71d293ecae689a0fd4a02e9fc370f12030ed
igoogolx <27353191+igoogolx@users.noreply.github.com>
2025-01-06 00:26:00 +0800

    v1.26.6 (#68)
    * doc: update
    
    * ci: enable virusTotal
    
    * feat: upgrade core
    
    * chore: v1.26.6

--- scripts/init.dart
+++ scripts/init.dart
@@ -13,7 +13,7 @@ final assetsPath =
 final binDir = Directory(path.join(assetsPath, 'bin'));
 
 const rawCoreName = 'itun2socks';
-const rawCoreVersion = '1.23.3';
+const rawCoreVersion = '1.23.5';
 
 Future downloadLatestCore(String arch, String token) async {
   final dio = Dio();
