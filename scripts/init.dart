db93cb3e9ea3a3e631b823c59d059aac4715d17f
igoogolx <27353191+igoogolx@users.noreply.github.com>
2025-02-15 18:04:38 +0800

    v1.29.1-beat.0 (#86)
    * feat: upgrade core
    
    * chore: v1.29.1-beat.0

--- scripts/init.dart
+++ scripts/init.dart
@@ -13,7 +13,7 @@ final assetsPath =
 final binDir = Directory(path.join(assetsPath, 'bin'));
 
 const rawCoreName = 'itun2socks';
-const rawCoreVersion = '1.27.0';
+const rawCoreVersion = '1.27.1';
 
 Future downloadLatestCore(String arch, String token) async {
   final dio = Dio();
