f3c601d2d94b3f0e9d7bebbf8d659c6e2cdb5c0e
igoogolx <27353191+igoogolx@users.noreply.github.com>
2025-02-23 18:47:41 +0800

    v1.29.2-beat.0 (#89)
    * chore: update rawCoreVersion to 1.27.4-beat.0
    
    * chore: update dependencies and versions in pubspec.lock and pubspec.yaml
    
    * chore: bump version to 1.29.2-beat.0 in pubspec.yaml

--- scripts/init.dart
+++ scripts/init.dart
@@ -13,7 +13,7 @@ final assetsPath =
 final binDir = Directory(path.join(assetsPath, 'bin'));
 
 const rawCoreName = 'itun2socks';
-const rawCoreVersion = '1.27.3';
+const rawCoreVersion = '1.27.4-beat.0';
 
 Future downloadLatestCore(String arch, String token) async {
   final dio = Dio();
