d242b3258c819012511322cf1d47d75e141ae638
igoogolx <27353191+igoogolx@users.noreply.github.com>
2025-03-28 18:47:34 +0800

    fix: not responding when os sleep on macOS

--- lib/home.dart
+++ lib/home.dart
@@ -64,6 +64,13 @@ class _HomeState extends State<Home> with WindowListener, TrayListener {
       setState(() {
         hasError = true;
       });
+    }, () async {
+      if(Platform.isMacOS){
+        var isFullScreen = await windowManager.isFullScreen();
+        if (isFullScreen){
+          windowManager.minimize();
+        }
+      }
     });
     coreManager?.run();
 
