74bfa4e1a9f4885756104b4662b0bd02e55bfa2a
igoogolx <27353191+igoogolx@users.noreply.github.com>
2025-04-05 18:35:04 +0800

    chore: update rawCoreVersion to 1.28.2

--- scripts/init.dart
+++ scripts/init.dart
@@ -13,7 +13,7 @@ final assetsPath =
 final binDir = Directory(path.join(assetsPath, 'bin'));
 
 const rawCoreName = 'itun2socks';
-const rawCoreVersion = '1.28.2-beat.1';
+const rawCoreVersion = '1.28.2';
 
 
 
