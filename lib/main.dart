a67492e6534c2e44883eb87be5095b5b4a839c19
igoogolx <27353191+igoogolx@users.noreply.github.com>
2025-05-14 22:15:18 +0800

    refactor: elevate in ProcessManager

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,10 @@
-import 'dart:io';
-
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:lux/app.dart';
-import 'package:lux/checksum.dart';
 import 'package:lux/const/const.dart';
 import 'package:lux/core_config.dart';
-import 'package:lux/elevate.dart';
 import 'package:lux/notifier.dart';
 import 'package:lux/tr.dart';
 import 'package:lux/utils.dart';
-import 'package:path/path.dart' as path;
 import 'package:window_manager/window_manager.dart';
 
 void main(args) async {
@@ -19,19 +13,6 @@ void main(args) async {
 
   try {
     await windowManager.ensureInitialized();
-    var corePath = path.join(Paths.assetsBin.path, LuxCoreName.name);
-    verifyCoreBinary(corePath);
-    if (Platform.isMacOS && !kDebugMode) {
-      var owner = await getFileOwner(corePath);
-      if (owner != "root") {
-        var i10nLabel = await getInitI10nLabel();
-        var code = await elevate(corePath, i10nLabel.macOSElevateServiceInfo);
-        if (code != 0) {
-          notifier.show(i10nLabel.macOSElevateServiceInfo);
-          exitApp();
-        }
-      }
-    }
 
     WindowOptions windowOptions = const WindowOptions(
       size: Size(800, 650),
