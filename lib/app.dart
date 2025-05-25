203b8a484c20c5220256cdf38b9d23017b17c56a
igoogolx <27353191+igoogolx@users.noreply.github.com>
2025-05-25 19:54:51 +0800

    fix: invalid AppLocalizations

--- lib/app.dart
+++ lib/app.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:lux/home.dart';
 import 'package:lux/tr.dart';
 import 'package:provider/provider.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+import 'l10n/app_localizations.dart';
 
 class App extends StatelessWidget {
   final String theme;
--- lib/tr.dart
+++ lib/tr.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+import 'l10n/app_localizations.dart';
 
 BuildContext? buildContext;
 
