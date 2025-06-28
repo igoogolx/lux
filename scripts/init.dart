3e76db715950f825ebf6bc4b6fc73692f96f5c19
igoogolx <27353191+igoogolx@users.noreply.github.com>
2025-06-28 03:13:58 -0700

    chore: update rawCoreVersion to 1.29.0-beat.0

--- scripts/init.dart
+++ scripts/init.dart
@@ -13,7 +13,7 @@ final assetsPath =
 final binDir = Directory(path.join(assetsPath, 'bin'));
 
 const rawCoreName = 'itun2socks';
-const rawCoreVersion = '1.28.9';
+const rawCoreVersion = '1.29.0-beat.0';
 
 Future<void> downloadFileWith(String url, String savePath) async {
   final dio = Dio();
