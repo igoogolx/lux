42f9c083abb979dfcfadda850f6251d30b52649b
igoogolx <27353191+igoogolx@users.noreply.github.com>
2025-08-10 13:54:55 +0800

    feat: format min bytes to KB

--- lib/utils.dart
+++ lib/utils.dart
@@ -145,7 +145,6 @@ Future<void> checkForUpdate() async {
 }
 
 String formatBytes(int bytes) {
-  if (bytes < 1024) return '$bytes B';
   if (bytes < 1024 * 1024) return '${(bytes / 1024).toStringAsFixed(1)} KB';
   if (bytes < 1024 * 1024 * 1024) {
     return '${(bytes / (1024 * 1024)).toStringAsFixed(1)} MB';
