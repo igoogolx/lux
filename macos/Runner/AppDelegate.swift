f0ea04715b9659679dd1f816291007f5307e68af
igoogolx <27353191+igoogolx@users.noreply.github.com>
2025-09-04 08:56:43 +0800

    fix: fail to clean proxy config when exited on macOS

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -1,19 +1,13 @@
 import Cocoa
 import FlutterMacOS
-import flutter_desktop_sleep
 
 @main
 class AppDelegate: FlutterAppDelegate {
-  var _windowManager = FlutterDesktopSleepPlugin()
 
   override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
     return false
   }
 
-   override func applicationShouldTerminate(_ sender: NSApplication) -> NSApplication.TerminateReply {
-    let controller : FlutterViewController = mainFlutterWindow?.contentViewController as! FlutterViewController
-    return _windowManager.applicationShouldTerminate(controller);
-    }
 
     override func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
       return true
