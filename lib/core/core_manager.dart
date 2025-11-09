1b2b5c36c69869ec0d44a05868a608867e6eed50
igoogolx <27353191+igoogolx@users.noreply.github.com>
2025-11-09 20:28:23 +0800

    fix: invalid CoreRunError

--- lib/core/core_manager.dart
+++ lib/core/core_manager.dart
@@ -5,6 +5,7 @@ import 'dart:io';
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/foundation.dart';
+import 'package:lux/error.dart';
 import 'package:lux/util/process_manager.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
 
