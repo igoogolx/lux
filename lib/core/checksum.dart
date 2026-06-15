a09aef111b4de9674b0843c93237bf79040fc1aa
miguelAngelo1999 <miguelAngelo1999@users.noreply.github.com>
2026-06-15 14:16:44 -0300

    chore: update checksum for latest lux_core binary

--- lib/core/checksum.dart
+++ lib/core/checksum.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 import 'package:crypto/crypto.dart';
 
 // checksum-start
- const darwinAmd64Checksum = "a9b1821a90ed3d07ab0afe0473c1ac6136ef70a671639685f8afd9eed9c4ae94";
+ const darwinAmd64Checksum = "08cb6dd70437ea727176f3ee79ef3970f7e3dfa2c31d99173844de78de63b7c8";
  const darwinArm64Checksum = "831573b8031a3c27cc4838bd484305603242c5a68cdd00800c33fe9b4b9a3ad7";
  const windowsAmd64Checksum = "34eb20f1f82baef43a3ed0a536a5d2db907f6d814c1dab0568004a7ea16c9c8a";
 // checksum-end
