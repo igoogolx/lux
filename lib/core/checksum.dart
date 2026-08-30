6e216c9a9e909601fe4bb12358484b111e05e744
igoogolx <27353191+igoogolx@users.noreply.github.com>
2026-08-31 00:54:35 +0800

    feat: upgrade core

--- lib/core/checksum.dart
+++ lib/core/checksum.dart
@@ -3,9 +3,9 @@ import 'dart:io';
 import 'package:crypto/crypto.dart';
 
 // checksum-start
- const darwinAmd64Checksum = "10806e47de5132a2594370cfff8609d462a0e51d80fd0d8fd0f192fffb416084";
- const darwinArm64Checksum = "8bc47e12887f7e700d03b9d332f0099e8b1db3c46d3de11cb49bf5620acb33f1";
- const windowsAmd64Checksum = "322e1431717d74224813d193a31a3350154410daeba581e7190fda80f79a72d4";
+ const darwinAmd64Checksum = "51c78411ef1dd3a5428af5e790d0fc27fdda746377a8157fc3aaca3f109b82a1";
+ const darwinArm64Checksum = "b8bb69a129e9caa70382c75b74f8ca8b778d4473ee25b539f4cf5a18d60b5292";
+ const windowsAmd64Checksum = "e04c5be65f4bf73cc3d9b0553aab0beaed0419a28a97f57bd575fd79fe8ed436";
 // checksum-end
 
 Future<void> verifyCoreBinary(String filePath) async {
